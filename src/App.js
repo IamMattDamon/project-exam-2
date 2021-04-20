a5acdf53f9423066eea6d02ed44af310bf6b7295
IamMattDamon <frochi@gmail.com>
2021-04-20 16:47:15 +0200

    Created authcontext, navigation and heading

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Container from "react-bootstrap/Container";
 import "./sass/style.scss";
 
 function App() {
---
+++ src/components/layout/Navigation.js
@@ -0,0 +1,53 @@
+import { useContext } from "react";
+import { Link, useHistory } from "react-router-dom";
+import AuthContext from "./../../context/AuthContext";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "./../../images/holidaze_logo_darkgbg.png";
+
+export default function Navigation() {
+    const [auth, setAuth] = useContext(AuthContext);
+
+    const history = useHistory();
+    console.log(auth);
+
+    function logout() {
+        setAuth(null);
+        history.push("/");
+    }
+
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <div className="container-fluid">
+                <NavBar.Brand>
+                    <img src={logo} alt="holidaze logo" className="nav__logo" />
+                </NavBar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav classname="mr-auto">
+                        <Link to="/" exact className="nav-link">
+                            Home
+                        </Link>
+                        <Link to="/accommodation" exact className="nav-link">
+                            Accommodation
+                        </Link>
+                        <Link to="/booking" exact className="nav-link">
+                            Booking
+                        </Link>
+                        <Link to="/contact" exact className="nav-link">
+                            Contact
+                        </Link>
+                        {auth ? (
+                        <>
+                            <Link to="/admin" exact className="nav-link">Admin</Link> 
+                            <button onClick={logout} className="btn-sm btn-primary ml-lg-3 py-0 mr-auto mt-3 mt-lg-0 logout-btn">Log out</button>
+                        </>
+                        ) : (
+                            <Link to="/login" exact className="nav-link">Login</Link>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </div>
+        </Navbar>
+    )
+}
