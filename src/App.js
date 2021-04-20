a771e7f95c297f9cb5868dd377a71f1c5bb36253
IamMattDamon <frochi@gmail.com>
2021-04-20 17:48:31 +0200

    Codebase and App.js set up complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+import Navigation from "./components/layout/Navigation";
+import Home from "./components/pages/Home";
+import Accommodation from "./components/pages/Accommodation";
+import Contact from "./components/pages/Contact";
+import Login from "./components/pages/Login";
+import Admin from "./components/pages/Admin";
+import { AuthProvider } from "./context/AuthContext";
 import "./sass/style.scss";
 
 function App() {
   return (
     <>
+    <AuthProvider>
+      <Router>
+        <Navigation />
+          <Container>
+            <Route path="/" exact component={Home}></Route>
+            <Route path="/accommodation">
+              <Accommodation />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/admin">
+              <Admin />
+            </Route>
+          </Container>
+      </Router>
+    </AuthProvider>
     </>
   );
 }
