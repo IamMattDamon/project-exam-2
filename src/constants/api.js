538f300ecd154bc1a78f21bc789a33fb7273de98
IamMattDamon <frochi@gmail.com>
2021-04-27 21:51:15 +0200

    base url set and header images imported

---
+++ src/constants/api.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "https://holidaze-heroku.herokuapp.com";
+export const ACCOMMODATIONS_URL = "https://holidaze-heroku.herokuapp.com/accommodations";
+export const ACTIVITIES_URL = "https://holidaze-heroku.herokuapp.com/activities";
