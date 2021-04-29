5e8767248e62d789719c4ff90be7c2a150ca9ad4
IamMattDamon <frochi@gmail.com>
2021-04-29 17:02:57 +0200

    api change and AccommodationList.js created

---
+++ src/components/accommodations/AccommodationList.js
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import AccommodationItem from "./AccommodationItem";
+import { BASE_URL } from "./../../constants/api";
+
+function AccommodationList() {
+  const [accommodations, setAccommodations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const response = (await fetch(BASE_URL)) + "/accommodations";
+
+        if (response.ok) {
+          const json = await response.json();
+          console.log(json);
+          setAccommodations(json.data);
+        } else {
+          setError("An error ocurred");
+        }
+      } catch (error) {
+        setError(error.toString());
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>An error ocurred: {error}</div>;
+  }
+
+  return (
+    <div className="accommodations-list d-flex flex-wrap justify-content-center">
+      {accommodations.map(function (accommodation) {
+        const {
+          id,
+          name,
+          type,
+          description,
+          bathrooms,
+          bedrooms,
+          size,
+          image,
+          feature_one,
+          feature_two,
+          feature_three,
+        } = accommodation;
+        return (
+          <AccommodationItem
+            key={id}
+            id={id}
+            name={name}
+            type={type}
+            description={description}
+            bathrooms={bathrooms}
+            bedrooms={bedrooms}
+            size={size}
+            image={image}
+            feature_one={feature_one}
+            feature_two={feature_two}
+            feature_three={feature_three}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default AccommodationList;
--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,1 @@
 export const BASE_URL = "https://holidaze-heroku.herokuapp.com";
-export const ACCOMMODATIONS_URL = "https://holidaze-heroku.herokuapp.com/accommodations";
-export const ACTIVITIES_URL = "https://holidaze-heroku.herokuapp.com/activities";
