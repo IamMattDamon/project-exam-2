87913dc56da7e19b61d21cbaebfed6e70194c0a6
IamMattDamon <frochi@gmail.com>
2021-05-03 20:22:26 +0200

    Footer done, only media coding left maybe link

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -7,91 +7,86 @@ export default function Footer() {
     <footer id="footer" className="main-footer">
       <Container>
         <Row>
-          <div className="col-xs-12 col-sm-6 col-md-3">
+          <div className="col-xs-12 col-sm-6 col-md-3 mt-4">
             <div className="footer-title-container">
-              <h4 className="footer-title">Holidaze</h4>
+              <h4 className="footer-title mt-4">Holidaze</h4>
             </div>
             <div className="footer-link-container">
               <ul className="footer-link-ul">
                 <li className="footer-link-li">
                   <div className="footer-link-content">
-                    <a href="#.">About</a>
+                    <a href="#." className="footer-link-a">About</a>
                   </div>
                 </li>
                 <li className="footer-link-li">
                   <div className="footer-link-content">
-                    <a href="#.">Careers</a>
+                    <a href="#." className="footer-link-a">Careers</a>
                   </div>
                 </li>
                 <li className="footer-link-li">
                   <div className="footer-link-content">
-                    <a href="#.">Sustainability</a>
-                  </div>
-                </li>
-                <li className="footer-link-li">
-                  <div className="footer-link-content">
-                    <a href="#.">Affiliate Info</a>
+                    <a href="#." className="footer-link-a">Affiliate Info</a>
                   </div>
                 </li>
               </ul>
             </div>
           </div>
 
-          <div className="col-xs-12 col-sm-6 col-md-3">
+          <div className="col-xs-12 col-sm-6 col-md-3 mt-4">
             <div className="footer-title-container">
-              <h4 className="footer-title">Contact</h4>
+              <h4 className="footer-title mt-4">Contact</h4>
             </div>
             <div className="footer-link-container">
               <ul className="footer-link-ul">
                 <li className="footer-link-li">
-                  <div className="footer-li-content">
-                    <a href="#.">Contact Admin</a>
+                  <div className="footer-link-content">
+                    <a href="#." className="footer-link-a">Contact Admin</a>
                   </div>
                 </li>
                 <li className="footer-link-li">
-                  <div className="footer-li-content">
-                    <a href="#.">Help/FAQ</a>
+                  <div className="footer-link-content">
+                    <a href="#." className="footer-link-a">Help/FAQ</a>
                   </div>
                 </li>
               </ul>
             </div>
           </div>
 
-          <div className="col-xs-12 col-sm-6 col-md-3">
+          <div className="col-xs-12 col-sm-6 col-md-3 mt-4">
             <div className="footer-title-container">
-              <h4 className="footer-title">Privacy</h4>
+              <h4 className="footer-title mt-4">Privacy</h4>
             </div>
             <div className="footer-link-container">
               <ul className="footer-link-ul">
                 <li className="footer-link-li">
-                  <div className="footer-li-content">
-                    <a href="#.">Terms &amp; Conditions</a>
+                  <div className="footer-link-content">
+                    <a href="#." className="footer-link-a">Terms &amp; Conditions</a>
                   </div>
                 </li>
                 <li className="footer-link-li">
-                  <div className="footer-li-content">
-                    <a href="#.">Privacy Policy</a>
+                  <div className="footer-link-content">
+                    <a href="#." className="footer-link-a">Privacy Policy</a>
                   </div>
                 </li>
               </ul>
             </div>
           </div>
 
-          <div className="col-xs-12 col-sm-6 col-md-3">
+          <div className="col-xs-12 col-sm-6 col-md-3 mt-4">
             <div className="footer-title-container">
-              <h4 className="footer-title">Social Media</h4>
+              <h4 className="footer-title mt-4">Social Media</h4>
             </div>
             <div className="footer-link-container">
               <ul className="footer-link-ul">
                 <li className="footer-link-li">
-                  <div className="footer-li-content">
-                    <span>
+                  <div className="footer-link-content">
+                    <span className="mr-2">
                       <FaFacebookSquare className="footer-icons" />
                     </span>
-                    <span>
+                    <span className="mr-2">
                       <FaInstagramSquare className="footer-icons" />
                     </span>
-                    <span>
+                    <span className="mr-2">
                       <FaPinterestSquare className="footer-icons" />
                     </span>
                     <span>
@@ -105,10 +100,10 @@ export default function Footer() {
         </Row>
       </Container>
       <div className="footer-copyright-container">
-        <h6 className="footer-copyright">
+        <p className="footer-copyright-text mt-3 mb-0 text-center">
           Copyright &#169;Holidaze 2021 | A project exam at Noroff by Frode
           Karlsen
-        </h6>
+        </p>
       </div>
     </footer>
   );
