466c8be8eb027a872dcdf9e6f05ec2e05e5db6b1
IamMattDamon <frochi@gmail.com>
2021-05-04 17:29:51 +0200

    home.js public transport section complete

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,6 +3,9 @@ import Footer from "./../layout/Footer";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import { BiChevronLeftCircle } from "react-icons/bi";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import FeaturedList from "./../accommodations/FeaturedList";
 import ActivitiesList from "./../activities/ActivitiesList";
 
@@ -48,8 +51,66 @@ export default function Home() {
           <section>
             <div className="home-content-container">
               <h2 className="content-heading mt-5 text-center">Public Transport</h2>
-              <h3 className="content-subheading text-center">Links to details for public transport in Bergen city</h3>
-            </div>
+              <h3 className="content-subheading text-center">Useful links for public transport in Bergen</h3>
+                <Container className="py-2 px-4 transport-container mt-4">
+                <div className="d-flex flex-wrap justify-content-center">
+                  <Row className="transport-list">
+                    <Col sm={4}>
+                      <a target="_blank" href="https://www.skyss.no/" rel="noreferrer" className="ext-link-a">
+                        <span>
+                          <BiChevronLeftCircle />
+                        </span>
+                        <span>&nbsp;Skyss</span>
+                      </a>
+                    </Col>
+                    <Col sm={4}>
+                      <a target="_blank" href="https://avinor.no/en/airport/bergen-airport/to-and-from-the-airport/bus-and-taxi/bus" rel="noreferrer" className="ext-link-a">
+                        <span>
+                          <BiChevronLeftCircle />
+                        </span>
+                        <span>&nbsp;Airport Bus</span>
+                      </a>      
+                    </Col>
+                    <Col sm={4}>
+                      <a target="_blank" href="https://www.nor-way.no/en/#/?step=2" rel="noreferrer" className="ext-link-a">
+                        <span>
+                          <BiChevronLeftCircle />
+                        </span>
+                        <span>&nbsp;Coast Bus</span>
+                      </a>
+                    </Col>
+                  </Row>
+                  </div>
+                  <div className="d-flex flex-wrap justify-content-center">
+                  <Row className="transport-list">
+                    <Col sm={4}>
+                      <a target="_blank" href="https://www.floyen.no/en" rel="noreferrer" className="ext-link-a">
+                        <span>
+                          <BiChevronLeftCircle />
+                        </span>
+                        <span>&nbsp;Fløibanen</span>
+                      </a>
+                    </Col>
+                    <Col sm={4}>
+                      <a target="_blank" href="https://www.norled.no/en/expressboat/maps-and-schedules/" rel="noreferrer" className="ext-link-a">
+                        <span>
+                          <BiChevronLeftCircle />
+                        </span>
+                        <span>&nbsp;Norled</span>
+                      </a>
+                    </Col>
+                    <Col sm={4}>
+                      <a target="_blank" href="https://www.hurtigruten.no/havner/bergen/" rel="noreferrer" className="ext-link-a">
+                        <span>
+                          <BiChevronLeftCircle />
+                        </span>
+                        <span>&nbsp;Hurtigruten</span>
+                      </a>
+                    </Col>
+                  </Row>
+                  </div>
+                  </Container>
+                </div>
           </section>
         </main>
       </Container>
