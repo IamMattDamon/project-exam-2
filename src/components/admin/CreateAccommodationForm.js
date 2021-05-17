26d25ab0cc99fee12f8af172016f9e744d518ccc
IamMattDamon <frochi@gmail.com>
2021-05-17 22:11:55 +0200

    Accommodation form with post request done - style

---
+++ src/components/admin/CreateAccommodationForm.js
@@ -0,0 +1,231 @@
+import { useState, useContext } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import axios from "axios";
+import { ACCOMMODATION_URL } from "../../constants/api";
+import AuthContext from "../../context/AuthContext";
+import Alert from "react-bootstrap/Alert";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const url = ACCOMMODATION_URL;
+
+const schema = yup.object().shape({
+    name: yup
+    .string()
+    .required("Enter name/address of the accommodation")
+    .min(3, "Accommodation name must be at least 3 characters"),
+    type: yup
+    .string()
+    .required("Enter accommodation type, i.e cabin or penthouse")
+    .min(4, "Type must be at least 4 characters long"),
+    image: yup
+    .mixed()
+    .test("imageUploaded", "Upload a JPEG file to continue", (value) => !!value.length),
+    description: yup
+    .string()
+    .required("Enter description of new accommodation")
+    .min(20, "Description must contain at least 20 characters")
+    .max(100, "Description cannot contain more than 100 characters"),
+    bedrooms: yup
+    .number()
+    .required("Enter the amount of bedrooms"),
+    bathrooms: yup
+    .number()
+    .required("Enter the amount of bathrooms"),
+    size: yup
+    .number()
+    .required("Enter the size of the accommodations (m2)"),
+    feature_one: yup
+    .string()
+    .required("Enter feature number one, i.e Wi-Fi"),
+    feature_two: yup
+    .string()
+    .required("Enter feature number two, i.e parking"),
+    feature_three: yup
+    .string()
+    .required("Enter feature number three, i.e swimming pool"),
+    price: yup
+    .number()
+    .required("Enter cost per night (currency NOK)"),
+});
+
+export default function CreateAccommodationForm() {
+  const [file, setFile] = useState(null);
+  const [auth] = useContext(AuthContext);
+
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const sortInputChange = (event) => {
+    setFile(event.target.images[0]);
+  };
+
+  async function onSubmit(data) {
+    console.log(data);
+
+    const token = auth.jwt;
+
+    let formData = new FormData();
+    delete data["image"];
+
+    formData.append(`files.image`, file, file.name);
+    formData.append("data", JSON.stringify(data));
+
+    try {
+      axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
+      const response = await axios.post(url, data);
+      console.log("response", response.data);
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+
+  return (
+    <>
+      <div className="create-form-success">
+        {submitted && (
+          <Alert variant="success">
+            Your new accommodation has been successfully created!
+          </Alert>
+        )}
+      </div>
+      <Form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group controlId="createForm.ControlInput1">
+          <Form.Label className="create-label-name">Address/name</Form.Label>
+          <Form.Control
+            name="name"
+            placeholder="Enter accommodation name"
+            ref={register}
+          />
+          {errors.name && (
+            <span className="text-danger">{errors.name.message}</span>
+          )}
+        </Form.Group>
+        <Form.Group controlId="createForm.ControlInput2">
+          <Form.Label className="create-label-type">Type</Form.Label>
+          <Form.Control
+            name="type"
+            placeholder="Enter type of accommodation"
+            ref={register}
+          />
+          {errors.type && (
+            <span className="text-danger">{errors.type.message}</span>
+          )}
+        </Form.Group>
+        <Form.Group controlId="createForm.ControlInput3">
+          <Form.Label className="create-label-description">
+            Description
+          </Form.Label>
+          <Form.Control
+            name="description"
+            placeholder="Enter description (20 to 100 characters)"
+            ref={register}
+          />
+          {errors.description && (
+            <span className="text-danger">{errors.description.message}</span>
+          )}
+        </Form.Group>
+        <Form.Group controlId="createForm.ControlInput4">
+          <Form.Label className="create-label-bedrooms">Bedrooms</Form.Label>
+          <Form.Control
+            name="bedrooms"
+            placeholder="Enter the amount of bedrooms"
+            ref={register}
+          />
+          {errors.bedrooms && (
+            <span className="text-danger">{errors.bedrooms.message}</span>
+          )}
+        </Form.Group>
+        <Form.Group controlId="createForm.ControlInput5">
+          <Form.Label className="create-label-bathrooms">Bathrooms</Form.Label>
+          <Form.Control
+            name="bathrooms"
+            placeholder="bathrooms"
+            ref={register}
+          />
+          {errors.bathrooms && (
+            <span className="text-danger">{errors.bathrooms.message}</span>
+          )}
+        </Form.Group>
+        <Form.Group controlId="createForm.ControlInput6">
+          <Form.Label className="create-label-size">Size</Form.Label>
+          <Form.Control name="size" placeholder="size in m2" ref={register} />
+          {errors.size && (
+            <span className="text-danger">{errors.size.message}</span>
+          )}
+        </Form.Group>
+        <Form.Group controlId="createForm.ControlInput7">
+          <Form.Label className="create-label-feature">Feature one</Form.Label>
+          <Form.Control
+            name="feature_one"
+            placeholder="feature one"
+            ref={register}
+          />
+          {errors.feature_one && (
+            <span className="text-danger">{errors.feature_one.message}</span>
+          )}
+        </Form.Group>
+        <Form.Group controlId="createForm.ControlInput8">
+          <Form.Label className="create-label-feature">Feature two</Form.Label>
+          <Form.Control
+            name="feature_two"
+            placeholder="feature two"
+            ref={register}
+          />
+          {errors.feature_two && (
+            <span className="text-danger">{errors.feature_two.message}</span>
+          )}
+        </Form.Group>
+        <Form.Group controlId="createForm.ControlInput9">
+          <Form.Label className="create-label-feature">
+            Feature three
+          </Form.Label>
+          <Form.Control
+            name="feature_three"
+            placeholder="feature three"
+            ref={register}
+          />
+          {errors.feature_three && (
+            <span className="text-danger">{errors.feature_three.message}</span>
+          )}
+        </Form.Group>
+        <Form.Group controlId="createForm.ControlInput10">
+          <Form.Label className="create-label-price">Price</Form.Label>
+          <Form.Control
+            name="price"
+            placeholder="price per night (NOK)"
+            ref={register}
+          />
+          {errors.price && (
+            <span className="text-danger">{errors.price.message}</span>
+          )}
+        </Form.Group>
+        <img
+          src={file ? URL.createObjectURL(file) : defaultImage}
+          alt={file ? file.name : "Default image"}
+          height="100%"
+          width="100px"
+        />
+        <Form.Group controlId="createForm.ControlTextarea1">
+          <Form.Label className="create-label-image">Select image</Form.Label>
+          <Form.Control
+            name="image"
+            type="file"
+            ref={register}
+            onChange={sortInputChange}
+          />
+          {errors.image && (
+            <span className="text-danger">{errors.image.message}</span>
+          )}
+        </Form.Group>
+        <Button type="submit" className="create-btn-cta">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+}
+
---
+++ src/components/admin/CreateItem.js
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+
+function CreateItem({
+  id,
+  name,
+  type,
+  bathrooms,
+  bedrooms,
+  size,
+  image,
+}) {
+  return (
+    <div className="mt-4">
+      <Card className="accommodation-card mr-5 mb-4">
+        <div className="text-center">
+          <img
+            src={image[0].url}
+            alt={image[0].name}
+            className="accommodation-card-image"
+          />
+        </div>
+        <Card.Body className="d-flex flex-column justify-content-center dark-bg accommodation-card-body">
+          <Card.Title>
+            <h5 className="accommodation-title text-center">{name}</h5>
+          </Card.Title>
+          <Card.Subtitle>
+            <h6 className="accommodation-subtitle text-center">{type}</h6>
+          </Card.Subtitle>
+        </Card.Body>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item text-center accommodation-background-fuchsia font-color-white">
+            {bedrooms}&nbsp;Bedrooms
+          </li>
+          <li className="list-group-item text-center accommodation-background-fuchsia font-color-white">
+            {bathrooms}&nbsp;Bathrooms
+          </li>
+          <li className="list-group-item text-center accommodation-background-fuchsia font-color-white">
+            Size:&nbsp;{size}&nbsp;&#13217;
+          </li>
+        </ul>
+        <Link to={`/accommodations/${id}`} className="text-center">
+          <button className="btn-lg btn accommodation-detail-btn">
+            More details
+          </button>
+        </Link>
+      </Card>
+    </div>
+  );
+}
+
+CreateItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  image: PropTypes.array.isRequired,
+  bathrooms: PropTypes.number.isRequired,
+  bedrooms: PropTypes.number.isRequired,
+};
+
+export default CreateItem;
